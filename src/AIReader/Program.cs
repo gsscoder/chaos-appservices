ac6c220de8064f9969289a6ed548a7e488515b1a
Giacomo Stelluti Scala <gsscoder@gmail.com>
2021-03-06 17:11:34 +0100

    Added AIReader sample project

---
+++ src/AIReader/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new HttpClient() {
+            // It's good practice to have a paramtric timeout
+            Timeout = TimeSpan.FromMilliseconds(Settings.HttpTimeout)
+        };
+        var endPoint = new UriBuilder($"https://api.applicationinsights.io/v1/apps/{Settings.AIApplicationId}/events/traces");
+        endPoint.Query = $"timespan=P{Settings.Days}D";
+        var request = new HttpRequestMessage() {
+            RequestUri = endPoint.Uri,
+            Method = HttpMethod.Get
+        };
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        request.Headers.Add("x-api-key", Settings.AIApiKey);
+        Stream stream = null;
+        await client.SendAsync(request)
+            .ContinueWith(async withMessage => {
+                stream = (await withMessage.Result.Content.ReadAsStreamAsync());
+            });
+        var json = JObject.Parse(new StreamReader(stream).ReadToEnd());
+        Console.WriteLine(json);
+    }
+}
---
+++ src/AIReader/Settings.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+static class Settings
+{
+    static readonly IConfiguration _instance = 
+        new ConfigurationBuilder()
+            .SetBasePath(Path.GetDirectoryName(typeof(Program).Assembly.Location))
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+    public static ushort HttpTimeout => _instance.GetValue<ushort>("httpTimeout"); 
+    public static Guid AIApplicationId => _instance.GetValue<Guid>("aiApplicationId"); 
+    public static string AIApiKey => _instance.GetValue<string>("aiApiKey"); 
+    public static uint Days => _instance.GetValue<ushort>("daysAgo"); 
+}
